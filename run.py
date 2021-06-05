726d0016fd58d016ebb66b666669392110772824
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-06-05 12:09:04 +0100

    be explicit about what sort of plot is desired

--- run.py
+++ run.py
@@ -12,5 +12,7 @@ if __name__ == "__main__":
     lon = -1.108652                                                             # [°]
     spd = 20.0                                                                  # [kts]
 
-    # Sail the vessel ...
-    funcs.sail(lon, lat, spd)
+    # Sail the vessel (ignoring minor islands, sailing in steps lasting 12
+    # minutes and only plotting contours every 6 hours for 2 days in the local
+    # vacinity) ...
+    funcs.sail(lon, lat, spd, detailed = False, local = True, nth = 30, ntot = 240)
