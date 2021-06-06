7b5b2e160f32147821b8c18f6cb24dcab3c4449d
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-06-06 11:49:15 +0100

    use new list of lands instead

--- funcs/remove_lands.py
+++ funcs/remove_lands.py
@@ -1,17 +1,18 @@
-def remove_lands(poly, sfiles):
+def remove_lands(poly, lands, simp = 0.1):
     """Remove the parts of a [Multi]Polygon that lie on land
 
-    This function reads in a [Multi]Polygon and a list of Shapefiles that
-    contain records of land. Each record of land is subtracted from the
-    [Multi]Polygon so as to leave only the parts of the [Multi]Polygon that lie
-    on water.
+    This function reads in a [Multi]Polygon and a list of Polygons of land
+    masses. Each Polygon of land is subtracted from the [Multi]Polygon so as to
+    leave only the parts of the [Multi]Polygon that lie on water.
 
     Parameters
     ----------
     poly : shapely.geometry.multipolygon.MultiPolygon
             the input shape
-    sfiles : list of strings
-            the list of Shapefiles
+    lands : list of shapely.geometry.polygon.Polygon
+            the list of land masses
+    simp : float, optional
+            how much intermediary [Multi]Polygons are simplified by (in degrees)
 
     Returns
     -------
@@ -19,18 +20,10 @@ def remove_lands(poly, sfiles):
             the output shape
     """
 
-    # Import special modules ...
-    try:
-        import cartopy
-    except:
-        raise Exception("\"cartopy\" is not installed; run \"pip install --user Cartopy\"") from None
+    # Loop over land ...
+    for land in lands:
+        # Subtract this Polygon from the [Multi]Polygon ...
+        poly = poly.difference(land)
 
-    # Loop over Shapefiles ...
-    for sfile in sfiles:
-        # Loop over records ...
-        for record in cartopy.io.shapereader.Reader(sfile).records():
-            # Subtract the geometry for this record from the [Multi]Polygon ...
-            poly = poly.difference(record.geometry)
-
-    # Return polygon ...
-    return poly
+    # Return a simplified [Multi]Polygon ...
+    return poly.simplify(simp)
