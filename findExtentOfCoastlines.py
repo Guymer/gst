131039748c341e0e0b4d882b55d784af3d71599e
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-07-11 17:00:15 +0100

    modernize

--- findExtentOfCoastlines.py
+++ findExtentOfCoastlines.py
@@ -5,10 +5,13 @@ try:
     import cartopy
 except:
     raise Exception("\"cartopy\" is not installed; run \"pip install --user Cartopy\"") from None
+
+# Import my modules ...
 try:
-    import shapely
+    import pyguymer3
+    import pyguymer3.geo
 except:
-    raise Exception("\"shapely\" is not installed; run \"pip install --user Shapely\"") from None
+    raise Exception("\"pyguymer3\" is not installed; you need to have the Python module from https://github.com/Guymer/PyGuymer3 located somewhere in your $PYTHONPATH") from None
 
 # Initialize variables ...
 globalMaxY = 0.0                                                                # [°]
@@ -24,36 +27,28 @@ sfiles = [
 for sfile in sfiles:
     # Loop over records ...
     for record in cartopy.io.shapereader.Reader(sfile).records():
-        # Check the type of this geometry ...
-        if isinstance(record.geometry, shapely.geometry.polygon.Polygon):
-            # Loop over coordinates in the exterior ...
-            for coord in record.geometry.exterior.coords:
-                # Skip un-real points (that only exist to make the
-                # [Multi]Polygon valid in Shapely) ...
-                if abs(coord[0]) > 179.9 or abs(coord[1]) > 89.9:
-                    continue
+        # Skip bad records ...
+        if not record.geometry.is_valid:
+            print(f"WARNING: Skipping a piece of land in \"{sfile}\" as it is not valid.")
+            continue
+        if record.geometry.is_empty:
+            print(f"WARNING: Skipping a piece of land in \"{sfile}\" as it is empty.")
+            continue
 
-                # Update variables ...
-                globalMaxY = max(globalMaxY, coord[1])                          # [°]
-                globalMinY = min(globalMinY, coord[1])                          # [°]
-        elif isinstance(record.geometry, shapely.geometry.multipolygon.MultiPolygon):
-            # Loop over geometries ...
-            for geom in record.geometry.geoms:
-                # Check the type of this geometry ...
-                if isinstance(geom, shapely.geometry.polygon.Polygon):
-                    # Loop over coordinates in the exterior ...
-                    for coord in geom.exterior.coords:
-                        # Skip un-real points (that only exist to make the
-                        # [Multi]Polygon valid in Shapely) ...
-                        if abs(coord[0]) > 179.9 or abs(coord[1]) > 89.9:
-                            continue
+        # Loop over all the bad Natural Earth Polygons in this geometry ...
+        for badPoly in pyguymer3.geo.extract_polys(record.geometry):
+            # Loop over all the individual good Polygons that make up this bad
+            # Natural Earth Polygon ...
+            for goodPoly in pyguymer3.geo.extract_polys(pyguymer3.geo.remap(badPoly)):
+                # Loop over coordinates in the exterior of the good Polygon ...
+                for coord in goodPoly.exterior.coords:
+                    # Skip un-real points (that only exist to make the
+                    # [Multi]Polygon valid in Shapely) ...
+                    if abs(coord[0]) > 179.9 or abs(coord[1]) > 89.9:
+                        continue
 
-                        # Update variables ...
-                        globalMaxY = max(globalMaxY, coord[1])                  # [°]
-                        globalMinY = min(globalMinY, coord[1])                  # [°]
-                else:
-                    raise Exception(f"\"geom\" is a \"{repr(type(geom))}\"")
-        else:
-            raise Exception(f"\"record.geometry\" is a \"{repr(type(record.geometry))}\"")
+                    # Update variables ...
+                    globalMaxY = max(globalMaxY, coord[1])                      # [°]
+                    globalMinY = min(globalMinY, coord[1])                      # [°]
 
-print(f"Land goes from {globalMinY:.2f}° to {globalMaxY:.2f}°")
+print(f"Latitude goes from {globalMinY:.2f}° to {globalMaxY:.2f}°")
