e2347d976c4c627896520361a3cf17f2c5fe2a07
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-07-24 10:02:53 +0100

    use argparse; remove loop

--- README.md
+++ README.md
@@ -5,8 +5,46 @@ This project aims to show how a vessel sails around the globe.
 If you want to run the example script using a profiler and print out the top 25 most time-consuming functions then run:
 
 ```
-python3.9 -m cProfile -o results.out run.py
+python3.9 -m cProfile -o results.out run.py -1.0 50.7 20.0 --dur 0.09
 python3.9 -c 'import pstats; p = pstats.Stats("results.out"); p.sort_stats(pstats.SortKey.CUMULATIVE).print_stats(25)'
+```
+
+## `compareBufferResolutions.py`
+
+To generate the data needed by `compareBufferResolutions.py` run:
+
+```
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 10 --res 10m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 19 --res 10m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 37 --res 10m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 91 --res 10m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 181 --res 10m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 361 --res 10m
+```
+
+## `resolutionConvergence.py`
+
+To generate the data needed by `resolutionConvergence.py` run:
+
+```
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 10 --res 110m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 10 --res 50m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 10 --res 10m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 19 --res 110m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 19 --res 50m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 19 --res 10m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 37 --res 110m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 37 --res 50m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 37 --res 10m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 91 --res 110m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 91 --res 50m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 91 --res 10m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 181 --res 110m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 181 --res 50m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 181 --res 10m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 361 --res 110m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 361 --res 50m
+python3.9 run.py -1.0 50.7 20.0 --dur 0.09 --nang 361 --res 10m
 ```
 
 ## To Do
--- run.py
+++ run.py
@@ -3,35 +3,44 @@
 # Use the proper idiom in the main module ...
 # NOTE: https://docs.python.org/3.8/library/multiprocessing.html#multiprocessing-programming
 if __name__ == "__main__":
+    # Import standard modules ...
+    import argparse
+
     # Import my modules ...
     import funcs
 
-    # Define starting location (just outside Portsmouth Harbour), sailing speed
-    # (of an average vessel) and sailing duration ...
-    lon = -1.0                                                                  # [°]
-    lat = 50.7                                                                  # [°]
-    spd = 20.0                                                                  # [kts]
-    dur = 0.1                                                                   # [days]
+    # Create argument parser and parse the arguments ...
+    parser = argparse.ArgumentParser(allow_abbrev = False, description = "This script reads in a starting coordinate and a sailing speed and then calculates the maximum possible sailing distance on the surface of the Earth that the vessel can reach in the specified time.")
+    parser.add_argument("lon", help = "the longitude of the starting point (in degrees)", type = float)
+    parser.add_argument("lat", help = "the latitude of the starting point (in degrees)", type = float)
+    parser.add_argument("spd", help = "the speed of the vessel (in knots)", type = float)
+    parser.add_argument("--detailed", action = "store_true", help = "take account of minor islands")
+    parser.add_argument("--dur", default = 1.0, help = "the duration of the voyage (in days)", type = float)
+    parser.add_argument("--freqFillSimp", default = 25, help = "fill in and simplify the sailing contour every freqFillSimp iteration", type = int)
+    parser.add_argument("--freqLand", default = 100, help = "re-evaluate the relevant land every freqLand iteration", type = int)
+    parser.add_argument("--freqPlot", default = 50, help = "plot sailing contours every freqPlot iteration", type = int)
+    parser.add_argument("--local", action = "store_true", help = "the plot has only local extent")
+    parser.add_argument("--nang", default = 19, help = "the number of directions from each point that the vessel could sail in", type = int)
+    parser.add_argument("--plot", action = "store_true", help = "make a plot")
+    parser.add_argument("--prec", default = 100.0, help = "the precision of the calculation (in metres)", type = float)
+    parser.add_argument("--res", default = "110m", help = "the resolution of the Natural Earth datasets", type = str)
+    parser.add_argument("--tol", default = 1.0e-10, help = "the Euclidean distance that defines two points as being the same (in degrees)", type = float)
+    args = parser.parse_args()
 
-    # **************************************************************************
-
-    # Loop over number of angles ...
-    for nang in [10, 19, 37, 91, 181, 361]:
-        print(f"Running with {360 // (nang - 1):d}° wedges ...")
-
-        # Loop over Natural Earth resolutions ...
-        for res in ["110m", "50m", "10m"]:
-            # Sail the vessel (ignoring minor islands) ...
-            funcs.sail(
-                lon,
-                lat,
-                spd,
-                detailed = False,
-                dur = dur,
-                freqFillSimp = 25,
-                freqLand = 100,
-                freqPlot = 50,
-                local = True,
-                nang = nang,
-                res = res
-            )
+    # Sail the vessel ...
+    funcs.sail(
+        args.lon,
+        args.lat,
+        args.spd,
+        detailed = args.detailed,
+        dur = args.dur,
+        freqFillSimp = args.freqFillSimp,
+        freqLand = args.freqLand,
+        freqPlot = args.freqPlot,
+        local = args.local,
+        nang = args.nang,
+        plot = args.plot,
+        prec = args.prec,
+        res = args.res,
+        tol = args.tol
+    )
