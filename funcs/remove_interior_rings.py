5e4dd0905265998477e30ecb2da0d25daddbed18
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-09-07 18:50:49 +0100

    use new helper function

--- funcs/remove_interior_rings.py
+++ funcs/remove_interior_rings.py
@@ -1,4 +1,4 @@
-def remove_interior_rings(poly, kwArgCheck = None, tol = 1.0e-10):
+def remove_interior_rings(shape, kwArgCheck = None, tol = 1.0e-10):
     """Remove all interior rings from a [Multi]Polygon
 
     This function reads in a [Multi]Polygon and returns a [Multi]Polygon which
@@ -6,15 +6,15 @@ def remove_interior_rings(poly, kwArgCheck = None, tol = 1.0e-10):
 
     Parameters
     ----------
-    poly : shapely.geometry.polygon.Polygon, shapely.geometry.multipolygon.MultiPolygon
-            the input shape
+    shape : shapely.geometry.polygon.Polygon, shapely.geometry.multipolygon.MultiPolygon
+        the input shape
     tol : float, optional
-            the Euclidean distance that defines two points as being the same (in degrees)
+        the Euclidean distance that defines two points as being the same (in degrees)
 
     Returns
     -------
-    poly : shapely.geometry.polygon.Polygon, shapely.geometry.multipolygon.MultiPolygon
-            the output shape
+    shape : shapely.geometry.polygon.Polygon, shapely.geometry.multipolygon.MultiPolygon
+        the output shape
     """
 
     # Import special modules ...
@@ -23,6 +23,13 @@ def remove_interior_rings(poly, kwArgCheck = None, tol = 1.0e-10):
     except:
         raise Exception("\"shapely\" is not installed; run \"pip install --user Shapely\"") from None
 
+    # Import my modules ...
+    try:
+        import pyguymer3
+        import pyguymer3.geo
+    except:
+        raise Exception("\"pyguymer3\" is not installed; you need to have the Python module from https://github.com/Guymer/PyGuymer3 located somewhere in your $PYTHONPATH") from None
+
     # Check keyword arguments ...
     if kwArgCheck is not None:
         print(f"WARNING: \"{__name__}\" has been called with an extra positional argument")
@@ -30,27 +37,23 @@ def remove_interior_rings(poly, kwArgCheck = None, tol = 1.0e-10):
     # **************************************************************************
 
     # Check the input type ...
-    if isinstance(poly, shapely.geometry.polygon.Polygon):
+    if isinstance(shape, shapely.geometry.polygon.Polygon):
         # Return a Polygon made of just the exterior LinearRing ...
-        return shapely.geometry.polygon.Polygon(poly.exterior).simplify(tol)
+        return shapely.geometry.polygon.Polygon(shape.exterior).simplify(tol)
 
     # Check the input type ...
-    if isinstance(poly, shapely.geometry.multipolygon.MultiPolygon):
+    if isinstance(shape, shapely.geometry.multipolygon.MultiPolygon):
         # Initialize list ...
         polys = []
 
-        # Loop over geometries ...
-        for geom in poly.geoms:
-            # Check the geometry type ...
-            if isinstance(geom, shapely.geometry.polygon.Polygon):
-                # Append a Polygon made of just the exterior LinearRing ...
-                polys.append(shapely.geometry.polygon.Polygon(geom.exterior))
-            else:
-                raise Exception(f"\"geom\" is a \"{repr(type(geom))}\"") from None
+        # Loop over Polygons ...
+        for poly in pyguymer3.geo.extract_polys(shape):
+            # Append a Polygon made of just the exterior LinearRing ...
+            polys.append(shapely.geometry.polygon.Polygon(poly.exterior))
 
-        # Return a MultiPolygon made of Polygons made of just the exterior
+        # Return a [Multi]Polygon made of Polygons made of just the exterior
         # LinearRings ...
         return shapely.ops.unary_union(polys).simplify(tol)
 
     # Catch error ...
-    raise TypeError(f"\"poly\" is a \"{repr(type(poly))}\"") from None
+    raise TypeError(f"\"shape\" is a \"{repr(type(shape))}\"") from None
