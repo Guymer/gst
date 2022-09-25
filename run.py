62c003838f9f27e3ed5f024ed9aeea1ada9aee1a
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-09-25 16:14:55 +0100

    rename module to be consistent

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     import argparse
 
     # Import my modules ...
-    import funcs
+    import gst
 
     # Create argument parser and parse the arguments ...
     parser = argparse.ArgumentParser(allow_abbrev = False, description = "This script reads in a starting coordinate and a sailing speed and then calculates the maximum possible sailing distance on the surface of the Earth that the vessel can reach in the specified time.")
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Sail the vessel ...
-    funcs.sail(
+    gst.sail(
         args.lon,
         args.lat,
         args.spd,
