71368c45e6a7bd38d21b02024c6d1d3c44ca6571
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2024-04-20 09:30:21 +0100

    add a script to list all the generated "allLands.wkb.gz" files

---
+++ listGeneratedlands.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Use the proper idiom in the main module ...
+# NOTE: See https://docs.python.org/3.11/library/multiprocessing.html#the-spawn-and-forkserver-start-methods
+if __name__ == "__main__":
+    # Import standard modules ...
+    import glob
+
+    # **************************************************************************
+
+    # Loop over all generated "allLands.wkb.gz" files ...
+    for allLand in sorted(glob.glob("res=?_cons=?.??e???_tol=?.??e???/nang=*_prec=?.??e???/allLands.wkb.gz")):
+        # Create short-hands ...
+        partOne, partTwo, _ = allLand.split("/")
+        res, cons, tol = partOne.split("_")
+        nang, prec = partTwo.split("_")
+
+        # Print combination ...
+        print(f"{res}    {cons}    {tol}    {nang:8s}    {prec}")
